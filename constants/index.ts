799bb5d3d288cb7df4bde4a2148d017947b72d1d
Vasilis Georgitzikis <tzikis@gmail.com>
2022-06-14 15:38:27 +0300

    Changed the election contract address to my contract

--- constants/index.ts
+++ constants/index.ts
@@ -4,6 +4,7 @@
   }
   export const walletConnectSupportedNetworks: Networks = {
     // Add your network rpc URL here
+    0: "https://rinkeby.infura.io/v3/40c2813049e44ec79cb4d7e0d18de173",
     1: "https://ethereumnode.defiterm.io",
     3: "https://ethereumnode.defiterm-dev.net"
   };
@@ -12,4 +13,4 @@
   export const supportedMetamaskNetworks = [1, 3, 4, 5, 42];
 
   export const ALBT_TOKEN_ADDRESS = "0xc6869a93ef55e1d8ec8fdcda89c9d93616cf0a72";
-  export const US_ELECTION_ADDRESS = "0xA09fF4F39FD8553051ABf0188100b7C5A6dc5452";
+  export const US_ELECTION_ADDRESS = "0x5155bE53a3144BAf6D2D8a3123Ac1914d5FDF76F";
